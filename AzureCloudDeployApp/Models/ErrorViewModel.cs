816809545d9d2c7e42ca57117b80ecef6745412c
vu.truongp <vu.truongp@saigontechnology.com>
2024-06-10 18:49:16 +0700

    first commit

---
+++ AzureCloudDeployApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AzureCloudDeployApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
